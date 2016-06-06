93c414c07da1f839c6b7b0832f9b9e8d8d7f68ba
zhongyu <zhongyu2005@gmail.com>
2016-06-06 23:28:00 +0800

    first commit

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+header("content-type:text/html;charset=utf-8");
+date_default_timezone_set("PRC");
+error_reporting(E_ALL);
+ini_set('display_errors',true);
+ini_set('error_log', 'error.log');
+
+// ignore_user_abort(true);//忽略abort
+// set_time_limit(0);// 无限制的执行下去
+
+phpinfo();
